445ee1200c95665c42a603e84ff9e5d09ef52f88
Ian Britos Borgunder <IB319561@fi365.ort.edu.uy>
2026-01-15 19:03:29 -0300

    Se agregaron los exteriores de maia y los mejores momentos de Paulina y Lucia

--- WebMali/Web/obj/Debug/net8.0/Web.AssemblyInfo.cs
+++ WebMali/Web/obj/Debug/net8.0/Web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4a9cab5c2e8e87e3e5be13c59598b31a967decd6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+78d02f5a194af277d01655161fe944d038866497")]
 [assembly: System.Reflection.AssemblyProductAttribute("Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
